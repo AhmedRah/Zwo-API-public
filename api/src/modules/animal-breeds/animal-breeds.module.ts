f98a6f807f5d3a07d8ae2a22a79be98b26245a58
tdrapied <t.drapied@gmail.com>
2022-08-04 16:08:27 +0200

    fix: Fix project

--- api/src/modules/animal-breeds/animal-breeds.module.ts
+++ api/src/modules/animal-breeds/animal-breeds.module.ts
@@ -9,12 +9,3 @@ import { AnimalBreedsController } from './animal-breeds.controller';
   exports: [AnimalBreedsService],
 })
 export class AnimalBreedsModule {}
-import { Module } from '@nestjs/common';
-import { AnimalBreedsService } from './animal-breeds.service';
-import { animalBreedsProviders } from './animal-breeds.providers';
-
-@Module({
-  providers: [AnimalBreedsService, ...animalBreedsProviders],
-  exports: [AnimalBreedsService],
-})
-export class AnimalBreedsModule {}
